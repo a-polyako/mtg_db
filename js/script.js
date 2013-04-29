6ab199f3a19681f77b81cdfb9dec868d78f8b1b7
apolyakov <alex.polyakov@t3worldwide.com>
2013-04-29 00:50:20 -0400

    added card removal, sample hand, sample hand drawing, starting to populate card db

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ var seenTypes = [];
 var deck = {};
 var save_name;
 var run_val = 0.0;
+var draw_lshift = 30;
+var draw_tshift = 200;
+var shuffle_deck = [];
+var card_to_draw = 7;
 
 function cmc_sort(card_a, card_b){
 	return(cards_info[card_a]["cmc"] - cards_info[card_b]["cmc"]);
@@ -60,7 +64,7 @@ $(document).ready(function(){
 	 	else{
 	 		tshift = 15;	
 	 	}
-	 	$img = $('<img class ='+ searchData + '>');
+	 	$img = $('<img id=on_table_img class ='+ searchData + '>');
 	 	$img.css({'height':'150px', 'position':'absolute', 'top':tshift, 'left':lshift});
 	 	$img.attr('src', $('#results_body img').attr('src'));
 	 	$('#content').append($img);
@@ -90,7 +94,7 @@ $(document).ready(function(){
 		 	else{
 		 		tshift = 15;	
 		 	}
-		 	$img = $('<img class ='+ searchData + '>');
+		 	$img = $('<img id=on_table_img class ='+ searchData + '>');
 		 	$img.css({'height':'150px', 'position':'absolute', 'top':tshift, 'left':lshift});
 		 	$img.attr('src', $('#results_body img').attr('src'));
 		 	$('#content').append($img);
@@ -117,7 +121,7 @@ $(document).ready(function(){
 				tshift = 15;	
 			}
 			for(var j = 0; j < deck[sorted_deck[i]]; j++){	
-				$img = $('<img class ='+ sorted_deck[i] + '>');
+				$img = $('<img id=on_table_img class ='+ sorted_deck[i] + '>');
 			 	$img.css({'height':'150px','position':'absolute', 'top':tshift, 'left':lshift});
 			 	$img.attr('src', card_imgs[sorted_deck[i]]);
 			 	$('#content').append($img);
@@ -143,11 +147,11 @@ $(document).ready(function(){
 				tshift = 15;
 			}
 			for(var j = 0; j < deck[sorted_deck[i]]; j++){	
-				$img = $('<img class ='+ sorted_deck[i] + '>');
+				$img = $('<img id=on_table_img class ='+ sorted_deck[i] + '>');
 			 	$img.css({'height':'150px','position':'absolute', 'top':tshift, 'left':lshift});
 			 	$img.attr('src', card_imgs[sorted_deck[i]]);
 			 	$('#content').append($img);
-			 	tshift += 20;
+			 	tshift += 15;
 			}
 		}
 	 });
@@ -169,7 +173,7 @@ $(document).ready(function(){
 				tshift = 15;
 			}
 			for(var j = 0; j < deck[sorted_deck[i]]; j++){	
-				$img = $('<img class ='+ sorted_deck[i] + '>');
+				$img = $('<img id=on_table_img class ='+ sorted_deck[i] + '>');
 			 	$img.css({'height':'150px','position':'absolute', 'top':tshift, 'left':lshift});
 			 	$img.attr('src', card_imgs[sorted_deck[i]]);
 			 	$('#content').append($img);
@@ -200,8 +204,64 @@ $(document).ready(function(){
 	});
 	$("#sample_hand").click(function(){
 		$("#sample_hand_modal").modal('toggle');
-	})
+		$('#sample-hand-body').empty();   
+		draw_lshift = 30;
+		draw_tshift = 200;
+		for(var i = 0;i<seenTypes.length;i++){	
+			for(var j = 0;j<deck[seenTypes[i]];j++){
+				shuffle_deck.push(seenTypes[i]);
+			}
+		}
+		shuffle_deck = shuffleArray(shuffle_deck);
+		var sh_lshift = 30;	
+		if(shuffle_deck.length >= 7){
+			for(var i = 0; i < 7; i++){
+				$img = $('<img class ='+ shuffle_deck[i] + '>');
+			 	$img.css({'height':'150px','position':'absolute', 'top':20, 'left':sh_lshift});
+			 	$img.attr('src', card_imgs[shuffle_deck[i]]);
+			 	$('#sample-hand-body').append($img);
+			 	sh_lshift += 130;			
+			 }
+		}
+		else{
+			$('#sample-hand-body').append('<p>Not enough cards in your deck to make a sample hand!</p>');
+		}
+	});
+	$("#draw_card").click(function(){
+		console.log(card_to_draw);
+		console.log(shuffle_deck);
+		if(card_to_draw > 13){
+			alert("Card drawing limit reached");
+			card_to_draw = 7;
+		}
+		else{
+			$img = $('<img class ='+ shuffle_deck[card_to_draw] + '>');
+		 	$img.css({'height':'150px','position':'absolute', 'top':draw_tshift, 'left':draw_lshift});
+		 	$img.attr('src', card_imgs[shuffle_deck[card_to_draw]]);
+		 	$('#sample-hand-body').append($img);
+		 	draw_lshift += 130;	
+		 	card_to_draw++;	
+		}
+	});
+	$('#content').click(function(e){	
+	  var cname = e.target.getAttribute("class");
+	  console.log(cards_info[cname]["price"]);
+	  run_val -= cards_info[cname]["price"];
+	  document.getElementById("running_val").innerHTML = "Total Price: $" + (run_val.toFixed(2)).toString();
+      document.getElementById("content").removeChild(e.target);
+
+  	});
 });
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
 
 var cards_info ={
 	"Forest":{"cmc":0,"color":0,"name":"Forest","price":.05},
@@ -209,39 +269,63 @@ var cards_info ={
 	"Mountain":{"cmc":0,"color":0,"name":"Mountain","price":.05},
 	"Swamp":{"cmc":0,"color":0,"name":"Swamp","price":.05},
 	"Plains":{"cmc":0,"color":0,"name":"Plains","price":.05},
+	"Wasteland":{"cmc":0,"color":0,"name":"Wasteland","price":50.05},
+	"Mutavault":{"cmc":0,"color":0,"name":"Mutavault","price":34.14},
+	"Force of Will":{"cmc":5,"color":2,"name":"Force of Will","price":82.27}
 }
 
 var cards_html ={
-	"Forest":"<img src='./images/forest.jpg' align='left' /><div><ul><li>Forest</li><li>Basic Land</li> \
-<li>({T}: Add {G} to your mana pool.))</li> \
-<li>Legal in Vintage (Type 1)<br/> Legal in Legacy (Type 1.5)<br/>Legal in Extended (Type 1.X) <br/> \
+	"Forest":"<img src='./images/forest.jpg' align='left' /><div><ul><li><b>Forest</b></li><li>Basic Land</li> \
+<li>{T}: Add {G} to your mana pool.</li> \
+<li><br>Legal in Vintage (Type 1)<br> Legal in Legacy (Type 1.5)<br/>Legal in Extended (Type 1.X) <br/> \
 Legal in Standard (Type 2)<br/>Legal in Classic (MTGO) <br/>Legal in Commander <br/> \
 Legal in Modern</li></ul></div>",
-	"Island":"<img src='./images/island.jpg' align='left' /><div><ul><li>Island</li><li>Basic Land</li> \
-<li>({T}: Add {U} to your mana pool.))</li><li>Legal in Vintage (Type 1)<br/> \
+	"Island":"<img src='./images/island.jpg' align='left' /><div><ul><li><b>Island</b></li><li>Basic Land</li> \
+<li>{T}: Add {U} to your mana pool.</li><li>Legal in Vintage (Type 1)<br/> \
+<br>Legal in Legacy (Type 1.5)<br/>Legal in Extended (Type 1.X) <br/> \
+Legal in Standard (Type 2)<br/>Legal in Classic (MTGO) <br/> \
+Legal in Commander <br/>Legal in Modern</li></ul></div>",
+	"Plains":"<img src='./images/plains.jpg' align='left' /><div><ul><li><b>Plains</b></li><li>Basic Land</li> \
+<li>({T}: Add {W} to your mana pool.</li><li>Legal in Vintage (Type 1)<br/> \
+<br>Legal in Legacy (Type 1.5)<br/>Legal in Extended (Type 1.X) <br/> \
+Legal in Standard (Type 2)<br/>Legal in Classic (MTGO) <br/> \
+Legal in Commander <br/>Legal in Modern</li></ul></div>",
+	"Mountain":"<img src='./images/mountain.jpg' align='left' /><div><ul><li><b>Mountain</b></li><li>Basic Land</li> \
+<li>{T}: Add {R} to your mana pool.</li><li>Legal in Vintage (Type 1)<br/> \
+<br>Legal in Legacy (Type 1.5)<br/>Legal in Extended (Type 1.X) <br/> \
+Legal in Standard (Type 2)<br/>Legal in Classic (MTGO) <br/> \
+Legal in Commander <br/>Legal in Modern</li></ul></div>",
+	"Swamp":"<img src='./images/swamp.jpg' align='left' /><div><ul><li><b>Swamp</b></li><li>Basic Land</li> \
+<li>{T}: Add {B} to your mana pool.</li><li>Legal in Vintage (Type 1)<br/> \
 Legal in Legacy (Type 1.5)<br/>Legal in Extended (Type 1.X) <br/> \
 Legal in Standard (Type 2)<br/>Legal in Classic (MTGO) <br/> \
 Legal in Commander <br/>Legal in Modern</li></ul></div>",
-	"Plains":"<img src='./images/plains.jpg' align='left' /><div><ul><li>Island</li><li>Basic Land</li> \
-<li>({T}: Add {W} to your mana pool.))</li><li>Legal in Vintage (Type 1)<br/> \
-Legal in Legacy (Type 1.5)<br/>Legal in Extended (Type 1.X) <br/> \
-Legal in Standard (Type 2)<br/>Legal in Classic (MTGO) <br/> \
-Legal in Commander <br/>Legal in Modern</li></ul></div>",
-	"Mountain":"<img src='./images/mountain.jpg' align='left' /><div><ul><li>Island</li><li>Basic Land</li> \
-<li>({T}: Add {R} to your mana pool.))</li><li>Legal in Vintage (Type 1)<br/> \
-Legal in Legacy (Type 1.5)<br/>Legal in Extended (Type 1.X) <br/> \
-Legal in Standard (Type 2)<br/>Legal in Classic (MTGO) <br/> \
-Legal in Commander <br/>Legal in Modern</li></ul></div>",
-	"Swamp":"<img src='./images/swamp.jpg' align='left' /><div><ul><li>Island</li><li>Basic Land</li> \
-<li>({T}: Add {B} to your mana pool.))</li><li>Legal in Vintage (Type 1)<br/> \
-Legal in Legacy (Type 1.5)<br/>Legal in Extended (Type 1.X) <br/> \
-Legal in Standard (Type 2)<br/>Legal in Classic (MTGO) <br/> \
-Legal in Commander <br/>Legal in Modern</li></ul></div>"
+	"Wasteland":"<img src='./images/wasteland.jpg' align='left' /><div><ul><li><b>Wasteland</b></li>Land</li> \
+<li>{T}:Add {1} to your mana pool.<br>{T},Sacrifice Wasteland: Destroy target nonbasic land.</li><li> \
+<br>Legal in Vintage (Type 1)<br>Legal in Legacy (Type 1.5</br>Legal in Classic (MTGO) <br> \
+Legal in Commander</br></li></ul></div>",
+	"Mutavault":"<img src='./images/mutavault.jpg' align='left' /><div><ul><li><b>Mutavault</b></li>Land</li> \
+<li>{T}:Add {1} to your mana pool.<br>{1}:Mutavault becomes a 2/2 creature with all creature types until end of turn. It's still a land.</li><li> \
+<br>Legal in Modern<br>Legal in Commander</br></li></ul></div>",
+	"Force of Will":"<img src='./images/fow.jpg' align='left' /><div><ul><li><b>Force of Will</b></li>Instant</li> \
+<li>You may pay 1 life and exile a blue card from your hand rather than pay Force of Will's mana cost. \
+<br><br>Counter target spell.</li><br><li> \
+Legal in Vintage (Type 1)<br>Legal in Legacy (Type 1.5</br>Legal in Classic (MTGO) <br> \
+Legal in Commander</br></li></ul></div>",
+	"Cursecatcher":"<img src='./images/cursecatcher.jpg' align='left' /><div><ul><li><b>Cursecatcher</b></li>Creature - Merfolk</li> \
+<li>Sacrifice Cursecatcher: Counter target instant or sorcery spell unless its controlle pays {1}. \
+<br><br>1/1</li><br><li>Legal in Modern</li><li> \
+Legal in Vintage (Type 1)<br>Legal in Legacy (Type 1.5</br>Legal in Classic (MTGO) <br> \
+Legal in Commander</br></li></ul></div>",
 }
 var card_imgs = {
 	"Forest":"./images/forest.jpg",
 	"Island":"./images/island.jpg",
 	"Plains":"./images/plains.jpg",
 	"Swamp":"./images/swamp.jpg",
-	"Mountain":"./images/mountain.jpg"
+	"Mountain":"./images/mountain.jpg",
+	"Wasteland":"./images/wasteland.jpg",
+	"Mutavault":"./images/mutavault.jpg",
+	"Force of Will":"./images/fow.jpg",
+	"Cursecatcher":"./images/cursecatcher.jpg"
 }
